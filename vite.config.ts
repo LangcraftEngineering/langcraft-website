cdc9d531b2b18cae61f8927c23551491ddd6b2cc
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2025-05-17 15:17:45 +0800

    Update the base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/langcraft-site/",
+    base: "/langcraft-website/",
     plugins: [react()],
     optimizeDeps: {
         exclude: ["lucide-react"],

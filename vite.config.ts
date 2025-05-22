b3e5fa14d43f4980609bc2926146e0b4de863b9b
nadeeshaan <nadeeshaangunasinghe@gmail.com>
2025-05-22 23:22:46 +0800

    Use EmailJs Service for email sending

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import javascriptObfuscator from "vite-plugin-javascript-obfuscator";
+
 export default defineConfig({
     base: "/langcraft-website/",
-    plugins: [react()],
+    plugins: [
+        react(),
+        javascriptObfuscator({
+            options: {
+                compact: true,
+                controlFlowFlattening: true,
+                selfDefending: true,
+                debugProtection: true,
+            },
+        }),
+    ],
     optimizeDeps: {
         exclude: ["lucide-react"],
+    },
+    build: {
+        sourcemap: false,
     },
 });
